0d9d3d50ea5a2aa60ff178bf11a6d09653092c00
Matthewd340 <matthewd340@nycstudents.net>
2024-04-03 09:24:41 -0400

    Added repeat background script but it isn't working properly

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the pipes left
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+    }
+}
