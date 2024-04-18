9488c5063d01c0918b9a6b6c6baa559496d8fc78
Matthewd340 <matthewd340@nycstudents.net>
2024-04-18 08:59:20 -0400

    Makes everything stop moving upon death

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 1.0f;
+    BirdController birdScript;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,16 @@ public class MoveLeft : MonoBehaviour
     void Update()
     {
         //Moves the pipes left
+       
+        if(birdScript.isAlive == true)
+     {
         transform.Translate(Vector2.left * Time.deltaTime * speed);
+        CancelInvoke();
+     }   
+    }
+
+    void Awake ()
+    {
+        birdScript = GameObject.Find("Red_Bird_0").GetComponent<BirdController>();
     }
 }
